fe6f98ec0447d56858423f6ddb037c2044ed12cb
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-25 18:13:06 +0530

    [Mukul Dev] Add: This is stack queue function of LinkedList

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(56);
+            stack.Push(70);
+            stack.Push(30);
+            stack.Display();
+        }
+
+        //BST - Binary Search Tree
+        //Delegates
+    }
+}
